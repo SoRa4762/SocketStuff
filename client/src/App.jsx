97196b8a58ca6f7a0d885b90c31bba7829d1fc95
SoRa <sonamraisx007@gmail.com>
2024-12-01 11:49:17 +0545

    backend global host done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("http://192.168.20.55:3000");
 
 function App() {
   const [chats, setChats] = useState([]);
